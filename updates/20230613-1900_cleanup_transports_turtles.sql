fe0f35c194c1fa29c1365b3b004ea0d5c3a4585d
cressidagp <gcpigni@gmail.com>
2023-06-13 14:05:00 -0300

    A couple of them even has waypoints.

--- updates/20230613-1900_cleanup_transports_turtles.sql
+++ updates/20230613-1900_cleanup_transports_turtles.sql
@@ -1,2 +1,5 @@
 -- Dragonblight: delete creatures that should be in transports (turtles)
 DELETE FROM `creature_spawns` WHERE `entry` IN ( 31804, 31805, 31806, 31807 );
+
+-- 31807 even has waypoints
+DELETE FROM `creature_waypoints` WHERE `spawnid` IN ( 128411, 128412 );
