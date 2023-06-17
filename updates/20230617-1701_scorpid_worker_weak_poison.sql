f42ca4abeaceb07246187d886938b349a6578d7c
cressidagp <gcpigni@gmail.com>
2023-06-17 19:04:49 -0300

    Updated chance.

--- updates/20230617-1701_scorpid_worker_weak_poison.sql
+++ updates/20230617-1701_scorpid_worker_weak_poison.sql
@@ -1,2 +1,2 @@
 REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
-VALUES (3124, 4, 4, 0, 0.7, 1, 6751, 11, -1, 9000, 0, 0);
+VALUES (3124, 4, 4, 0, 10, 1, 6751, 11, -1, 9000, 0, 0);
