05863d0986bc5d58fc958b01c81e5fd6324f97ca
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2023-06-25 20:39:44 +0200

    Remove AhnQiraj war effort stuff from Ironforge

---
+++ updates/20230625-1838_remove_war_effort.sql
@@ -0,0 +1,11 @@
+-- War Effort Volunteer
+DELETE FROM creature_spawns WHERE entry = 15663;
+
+-- Master Sergeant Maclure
+DELETE FROM creature_spawns WHERE entry = 15708;
+
+-- Resource GOs
+DELETE FROM gameobject_spawns WHERE entry = 180598;
+DELETE FROM gameobject_spawns WHERE entry = 180679;
+DELETE FROM gameobject_spawns WHERE entry = 180680;
+DELETE FROM gameobject_spawns WHERE entry = 180681;
