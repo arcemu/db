3fa222be8153ef66dacc8319aa19f20d1f139cce
cressidagp <gcpigni@gmail.com>
2023-06-27 10:32:24 -0300

    Forget to remove quotes.

--- updates/20230627-0550_makrura_elder_ai_claw_grasp.sql
+++ updates/20230627-0550_makrura_elder_ai_claw_grasp.sql
@@ -1,3 +1,3 @@
 -- Makrura Elder: cast 'Claw Grasp'
-REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
 VALUES (3141, 4, 4, 0, 35, 0, 5424, 8, -1, 9000, 0, 0);
