81278f1357029a4090d7199d3563adb9cea94247
cressidagp <gcpigni@gmail.com>
2023-06-27 11:03:00 -0300

    Updated 'Deathguard Phillips' spawn position and movetype.

---
+++ updates/20230627-1605_deathguard_phillips_spawn_movetype.sql
@@ -0,0 +1,3 @@
+-- Deathguard Phillips (Deathknell)
+UPDATE creature_spawns SET position_x = 1847.25, position_y = 1572.01, position_z = 95.0795, 
+orientation = 4.79961, movetype = 2 WHERE entry = 1739;
