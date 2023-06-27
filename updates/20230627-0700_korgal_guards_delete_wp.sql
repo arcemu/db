2e612def2a5735ce8054c227a482b9272da71237
cressidagp <gcpigni@gmail.com>
2023-06-27 02:10:53 -0300

    Forget to set movetype to 0

--- updates/20230627-0700_korgal_guards_delete_wp.sql
+++ updates/20230627-0700_korgal_guards_delete_wp.sql
@@ -1,2 +1,3 @@
 -- 'Scarlet Convert' & 'Scarlet Initiate' at sides of 'Meven Kortal'
 DELETE FROM creature_waypoints WHERE spawnid IN ( 10835, 10846 );
+UPDATE creature_spawns SET movetype = 0 WHERE id IN ( 10835, 10846 );
