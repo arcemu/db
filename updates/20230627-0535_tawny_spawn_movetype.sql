4768e5b04972f1da72839a4c575c0ad112915135
cressidagp <gcpigni@gmail.com>
2023-06-27 00:33:05 -0300

    Updated 'Tawny Grisette' spawn coords and fixed movetype

---
+++ updates/20230627-0535_tawny_spawn_movetype.sql
@@ -0,0 +1,3 @@
+-- Tawny Grisette (Undercity)
+UPDATE creature_spawns SET position_x = 1622.25, position_y = 263.732, position_z = -43.1027, 
+orientation = 2.32365, movetype = 2 WHERE entry = 4554;
