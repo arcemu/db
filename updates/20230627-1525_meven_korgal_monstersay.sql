9bd0448cca71fae033237bb1922b5bd6db0fa9a8
cressidagp <gcpigni@gmail.com>
2023-06-27 10:25:24 -0300

    Added 'Meven Kortal' monstersay

---
+++ updates/20230627-1525_meven_korgal_monstersay.sql
@@ -0,0 +1,3 @@
+-- Meven Korgal monstersay OnEnterCombat
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (1667, 0, 100, 0, 12, 'Meven Korgal', 'The Scarlet Crusade shall not fail in its mission!', 'These lands shall be cleansed!', NULL, NULL, NULL);
