ec383a9a52d9a6dd59be515bbc0c89220c6dfdb1
cressidagp <gcpigni@gmail.com>
2023-07-09 09:03:23 -0300

    Added 'Frostmane Novice' monstersay onEnterCombat.

---
+++ updates/20230709-1402_frostmane_novice_monstersay.sql
@@ -0,0 +1,3 @@
+-- 'Frostmane Novice': monstersay onEnterCombat
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (946, 0, 20, 0, 12, 'Frostmane Novice', 'You be dead soon!', 'Killing you be easy.', 'I gonna make you into mojo!', 'My weapon be thirsty!', NULL);
