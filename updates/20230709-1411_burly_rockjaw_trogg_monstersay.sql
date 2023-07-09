928fc650e5ffefdb4bd7798047f0716487e93240
cressidagp <gcpigni@gmail.com>
2023-07-09 09:11:41 -0300

    Added 'Burly Rockjaw Trogg' monstersay onEnterCombat.

---
+++ updates/20230709-1411_burly_rockjaw_trogg_monstersay.sql
@@ -0,0 +1,3 @@
+-- 'Burly Rockjaw Trogg': monstersay onEnterCombat
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (724, 0, 20, 0, 12, 'Burly Rockjaw Trogg', 'Crush!', 'Kill!', 'Destroy!', NULL, NULL);
