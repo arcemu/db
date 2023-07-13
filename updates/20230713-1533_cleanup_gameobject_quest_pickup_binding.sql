a28a07c4ade6f1f3ce6d2c4dcee00045ecf88099
cressidagp <gcpigni@gmail.com>
2023-07-13 10:33:22 -0300

    Removed some dirt we added previously.

---
+++ updates/20230713-1533_cleanup_gameobject_quest_pickup_binding.sql
@@ -0,0 +1,4 @@
+-- Remove Quest binding for 'Galgars Cactus'
+DELETE FROM gameobject_quest_pickup_binding WHERE quest = 498;
+-- Remove Quest binding for 'Milly's harvest'
+DELETE FROM gameobject_quest_pickup_binding WHERE quest = 3904;
