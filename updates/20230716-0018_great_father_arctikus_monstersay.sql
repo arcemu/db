ada4b8901c851bd23595a25488895110e86d3e65
cressidagp <gcpigni@gmail.com>
2023-07-15 19:18:22 -0300

    Added 'Great Father Arctikus' monstersay onEnterCombat.

---
+++ updates/20230716-0018_great_father_arctikus_monstersay.sql
@@ -0,0 +1,3 @@
+-- 'Great Father Arctikus' monstersay onEnterCombat
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (1260, 0, 100, 14, 12, 'Great Father Arctikus', 'You dare defile Frostmane Hold, $R. Prepare to die!', 'Oh, we are going to get along just fine, $n.', NULL, NULL, NULL);
