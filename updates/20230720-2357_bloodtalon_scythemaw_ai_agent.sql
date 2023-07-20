4743e22daa91bd97324d9a52b7b168b6836a7601
cressidagp <gcpigni@gmail.com>
2023-07-20 18:58:28 -0300

    'Bloodtalon Scythemaw' will cast 'Rushing Charge Now.

---
+++ updates/20230720-2357_bloodtalon_scythemaw_ai_agent.sql
@@ -0,0 +1,3 @@
+-- Bloodtalon Scythemaw: cast 'Rushing Charge' onEnterCombat (long cooldown to avoid repeat)
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (3123, 4, 4, 0, 100, 0, 6268, 10, -1, 60000, 0, 0);
