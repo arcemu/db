d6996beae124f2c7ea3f9a4f793b444327579751
cressidagp <gcpigni@gmail.com>
2023-07-20 18:51:53 -0300

    Hotfix for #a6f2024c3838184f641e0b5fe099c9dafc989a62

--- updates/20230719-0502_quest_mazzranache_fixed_creature_loots.sql
+++ updates/20230719-0502_quest_mazzranache_fixed_creature_loots.sql
@@ -10,8 +10,8 @@ UPDATE loot_creatures SET normal10percentchance = 25.00, normal25percentchance =
 WHERE entryid = 2959 AND itemid = @ID1;
 
 -- Prairie Wolf Alpha
-INSERT INTO loot_creatures (`entryid`, `itemid`, `normal10percentchance`, `normal25percentchance`, `heroic10percentchance`, `heroic25percentchance`, `mincount`, `maxcount`) 
-VALUES (2960, @ID1, 25.00, 25.00, 25.00, 25.00, 1, 1);
+UPDATE loot_creatures SET normal10percentchance = 25.00, normal25percentchance = 25.00, heroic10percentchance = 25.00, heroic25percentchance = 25.00
+WHERE entryid = 2960 AND itemid = @ID1;
 
 -- Item: Flatland Cougar Femur
 SET @ID2 := 4805;
