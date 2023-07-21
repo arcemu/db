4d5aa5596381e9fb362b2ef2eef589f9f8fdac4b
cressidagp <gcpigni@gmail.com>
2023-07-21 11:37:19 -0300

    Added 'Palemane Skinner' monstersay onEnterCombat.

---
+++ updates/20230721-1638_palename_skinner_monstersay.sql
@@ -0,0 +1,2 @@
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (2950, 0, 30, 0, 12, 'Palemane Skinner', 'More bones to gnaw on...', 'Grrrr... fresh meat!', NULL, NULL, NULL);
