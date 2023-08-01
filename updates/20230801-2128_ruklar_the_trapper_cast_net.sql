5022449f3c84551d5c9ffbef663a25e1ec9bbd9c
cressidagp <gcpigni@gmail.com>
2023-08-01 16:29:44 -0300

    'Ruklar the Trapper' will trap players now.

---
+++ updates/20230801-2128_ruklar_the_trapper_cast_net.sql
@@ -0,0 +1,3 @@
+-- Ruklar the Trapper cast 'Net'
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (60, 4, 4, 0, 70, 0, 12024, 1, -1, 6000, 0, 0 );
