4b92707e90007478731f72310cf66447601dd59e
cressidagp <gcpigni@gmail.com>
2023-08-01 14:01:57 -0300

    There is not need of set maxcount since buff will not recast until left. Also we can just load default targetType and Coldown in this case.

---
+++ updates/20230801-1902_garrick_padfoot_ai_agent_changes.sql
@@ -0,0 +1,3 @@
+-- Garrick Padfoot: cast "Defensive Stance"
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (103, 4, 4, 0, 100, 0, 7164, 10, -1, -1, 0, 0);
