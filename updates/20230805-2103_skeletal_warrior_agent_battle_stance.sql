c8e70e1ec4c09faffc41d0c28a0c8feefe7ec2e6
cressidagp <gcpigni@gmail.com>
2023-08-05 17:04:34 -0300

    'Skeletal Warrior' should cast 'Battle Stance'.

---
+++ updates/20230805-2103_skeletal_warrior_agent_battle_stance.sql
@@ -0,0 +1,3 @@
+-- Skeletal Warrior: cast "Battle Stance"
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (48, 4, 4, 0, 100, 0, 7165, 10, -1, -1, 0, 0);
