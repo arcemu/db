e2762000a0314434cefc5d4be7880903f0a8ded1
cressidagp <gcpigni@gmail.com>
2023-08-05 11:08:58 -0300

    'Ruklar the Trapper' should cast 'Pierce Armor'.

---
+++ updates/20230805-1608_ruklar_the_trapper_cast_pierce_armor.sql
@@ -0,0 +1,3 @@
+-- Ruklar the Trapper cast 'Pierce Armor'
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (60, 4, 4, 0, 30, 0, 6016, 11, -1, -1, 0, 0 );
