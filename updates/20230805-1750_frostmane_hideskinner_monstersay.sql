9bcebbc9dcd46d8dd58723052128c90ebac535c6
cressidagp <gcpigni@gmail.com>
2023-08-05 12:50:55 -0300

    Added 'Frostmane Hideskinner' monstersay onEnterCombat.

---
+++ updates/20230805-1750_frostmane_hideskinner_monstersay.sql
@@ -0,0 +1,3 @@
+-- Frostmane Hideskinner monstersay onEnterCombat
+REPLACE INTO npc_monstersay (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (1122, 0, 20, 0, 12, 'Frostmane Hideskinner', 'You be dead soon!', 'Killing you be easy.', 'I gonna make you into mojo!', 'My weapon be thirsty!', NULL);
