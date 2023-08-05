7c6bb661a62f364f5cf9fff08777c086085ffb69
cressidagp <gcpigni@gmail.com>
2023-08-05 12:47:32 -0300

    Added 'Frostmane Snowstrider' monstersay onEnterCombat.

---
+++ updates/20230805-1747_frostmane_snowstrider_monstersay.sql
@@ -0,0 +1,3 @@
+-- Frostmane Snowstrider onEnterCombat
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (1121, 0, 20, 0, 12, 'Frostmane Snowstrider', 'My weapon be thirsty!', 'Killing you be easy.', 'You be dead soon!', 'I gonna make you into mojo!', NULL);
