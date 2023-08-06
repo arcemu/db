0564da095a793c1556cbe142417b8e7fa8f5b2c4
cressidagp <gcpigni@gmail.com>
2023-08-06 09:38:26 -0300

    'Night Web Matriarch' should cast 'Poison'.

---
+++ updates/20230806-1438_night_web_matriarch_agent_poison.sql
@@ -0,0 +1,3 @@
+-- Night Web Matriarch: cast 'Poison'
+REPLACE INTO ai_agents (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`) 
+VALUES (1688, 4, 4, 0, 15, 0, 744, 11, -1, 5000, 0, 0);
