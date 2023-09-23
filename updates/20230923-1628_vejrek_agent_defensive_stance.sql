3f16337af9b858fb91b9020495946ae5f789aef7
cressidagp <gcpigni@gmail.com>
2023-09-23 11:28:50 -0300

    Added 'Vejrek' ai_agent for cast 'Defensive Stance'.

---
+++ updates/20230923-1628_vejrek_agent_defensive_stance.sql
@@ -0,0 +1,3 @@
+-- Vejrek: cast "Defensive Stance" on combat, no repeat
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (6113, 4, 4, 0, 100, 0, 7164, 10, -1, -1, 0, 0, 'Vejrek');
