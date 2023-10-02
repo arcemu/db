352c76857034c46873c5f76fc74763338072d4c2
cressidagp <gcpigni@gmail.com>
2023-10-02 00:04:59 -0300

    I should use radians for follow angle.

---
+++ updates/20231002-0505_creature_formation_to_radian.sql
@@ -0,0 +1,17 @@
+DELETE FROM creature_formations WHERE target_spawn_id IN ( 40925, 1550, 28677 );
+
+-- Miss Danna
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28673, 28677, 2.0944, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28676, 28677, 5.23599, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28672, 28677, 1.0472, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28675, 28677, 5.75959, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28674, 28677, 1.5708, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28671, 28677, 4.71239, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (28670, 28677, 0.523599, 3);
+
+-- Edrick Killian
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (40927, 40925, 4.71239, 3);
+
+-- Porcine Entourage
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (1594, 1550, 1.0472, 3);
+INSERT INTO creature_formations (`spawn_id`, `target_spawn_id`, `follow_angle`, `follow_dist`) VALUES (1595, 1550, 4.18879, 3);
