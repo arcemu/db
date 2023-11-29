be1e401f90add155c82ecc2abbbf380e5dbf8cff
cressidagp <gcpigni@gmail.com>
2023-11-29 10:16:35 -0300

    Fixed 'Elly Langston' movetype and spawn point.

---
+++ updates/20231129-1416-elly_langston_spawnpoint_movetype.sql
@@ -0,0 +1,3 @@
+-- Elly Langston (spawnid = 9338)
+
+UPDATE creature_spawns SET position_x = -8608.97, position_y = 391.84, position_z = 102.92, orientation = 2.16253, movetype = 2 WHERE entry = 1328;
