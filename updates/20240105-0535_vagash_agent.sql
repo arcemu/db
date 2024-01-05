a200a78be93b3c9b25aada043342af74e5802a6f
cressidagp <gcpigni@gmail.com>
2024-01-05 02:01:51 -0300

    Fixed 'Vagash' agent since it was displaing an error on console.

---
+++ updates/20240105-0535_vagash_agent.sql
@@ -0,0 +1,3 @@
+-- Vagash cast 'Glacial Roar'
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`)
+VALUES (1388, 4, 4, 0, 20, 0, 3143, 3, 1, 0, 0, 0, 'Vagash');
