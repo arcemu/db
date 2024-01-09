3c92d88cc50f5fb8058cdd96a0d755965fcff0b6
cressidagp <gcpigni@gmail.com>
2024-01-09 12:02:00 -0300

    Fixed 'Hogger' charges once again.

---
+++ updates/20240109-1602_hogger_charges.sql
@@ -0,0 +1,3 @@
+-- Hogger cast 'Rushing Charge'
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (448, 4, 4, 0, 100, 0, 6268, 8, 4, 40000, 0, 0, 'Hogger');
