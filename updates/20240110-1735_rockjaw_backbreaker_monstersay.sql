3294942b1f5fdae940b0e0ad8a2c1f6adef6b75e
cressidagp <gcpigni@gmail.com>
2024-01-10 13:36:02 -0300

    Added 'Rockjaw Backbreaker' monstersay.

---
+++ updates/20240110-1735_rockjaw_backbreaker_monstersay.sql
@@ -0,0 +1,3 @@
+-- Rockjawb Backbreaker monstersay onCombat
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (1118, 0, 50, 0, 12, 'Rockjaw Bonesnapper', 'Kill!', 'Crush!', NULL, NULL, NULL);
