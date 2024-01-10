5635a56ee5d5b8c4e2cda366d618b6c9b04e14af
cressidagp <gcpigni@gmail.com>
2024-01-10 11:40:33 -0300

    Missing coma.

--- updates/20240110-1517_leper_gnome_agent_cleanup.sql
+++ updates/20240110-1517_leper_gnome_agent_cleanup.sql
@@ -1,3 +1,3 @@
 -- Lepper Gnomes should only cast 'Decayed Strenght'
-UPDATE creature_proto SET spell1 = 6951 WHERE entry = 1211
+UPDATE creature_proto SET spell1 = 6951 WHERE entry = 1211;
 DELETE FROM ai_agents WHERE entry = 1211 AND type = 4 AND spell != 6951;
