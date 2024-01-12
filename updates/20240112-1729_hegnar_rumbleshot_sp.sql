351644c0153efcdeb1b1a9cc132c76f3bcc996f0
cressidagp <gcpigni@gmail.com>
2024-01-12 13:32:21 -0300

    Forget to remove spawnid from the last commit.

--- updates/20240112-1729_hegnar_rumbleshot_sp.sql
+++ updates/20240112-1729_hegnar_rumbleshot_sp.sql
@@ -1,29 +1,17 @@
 -- Hegnar Rumbleshot
-SET @SPID := 9237;
-
 UPDATE creature_spawns SET position_x = -6015.51, position_y = -201.511, position_z = 407.437, orientation = 0.0349066 WHERE entry = 1243;
 
 -- Rifleman Middlecamp
-SET @SPID := 54158;
-
 UPDATE creature_spawns SET position_x = -6010.67, position_y = -199.434, position_z = 406.792, orientation = 5.53269 WHERE entry = 10804;
 
 -- Rifleman Wheeler
-SET @SPID := 54157;
-
 UPDATE creature_spawns SET position_x = -6008.53, position_y = -199.145, position_z = 406.687, orientation = 4.37218 WHERE entry = 10803;
 
 -- Spotter Klemmy
-SET @SPID := 54159;
-
 UPDATE creature_spawns SET position_x = -6007.3, position_y = -200.348, position_z = 406.777, orientation = 3.28222 WHERE entry = 10805;
 
 -- Shorty
-SET @SPID := 53866;
-
 UPDATE creature_spawns SET position_x = -6007.46, position_y = -202.158, position_z = 407.448, orientation = 2.47837 WHERE entry = 10611;
 
 -- Angus
-SET @SPID := 53865;
-
 UPDATE creature_spawns SET position_x = -6009.25, position_y = -202.65, position_z = 406.983, orientation = 1.58825 WHERE entry = 10610;
