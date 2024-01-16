376f1ec0e3bbd991e06c388ad6e396d7526c9735
cressidagp <gcpigni@gmail.com>
2024-01-16 17:24:05 -0300

    Gug Fatcandle: added weapon and fixed wrong entry.

--- custom/gug_fatcandle_spawn.sql
+++ custom/gug_fatcandle_spawn.sql
@@ -10,9 +10,9 @@ SET @SPID := 711267;
 UPDATE creature_names SET rank = 4 WHERE entry = 62;
 
 -- Spawn inside Echo Ridge
-DELETE FROM `creature_spawns` WHERE `entry` = 82;
+DELETE FROM `creature_spawns` WHERE `entry` = 62;
 INSERT INTO `creature_spawns` (`id`, `entry`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `movetype`, `displayid`, `faction`, `flags`, `bytes0`, `bytes1`, `bytes2`, `emote_state`, `npc_respawn_link`, `channel_spell`, `channel_target_sqlid`, `channel_target_sqlid_creature`, `standstate`, `death_state`, `mountdisplayid`, `slot1item`, `slot2item`, `slot3item`, `CanFly`, `phase`) 
-VALUES (@SPID, 62, 0, -8557.43, -213.36, 84.54, 0.45, 0, 26, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+VALUES (@SPID, 62, 0, -8557.43, -213.36, 84.54, 0.45, 0, 26, 25, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1904, 0, 0, 0, 1);
 
 -- Cast fireball from item?
 REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
@@ -20,7 +20,7 @@ VALUES (62, 4, 4, 2, 60, 0, 133, 8, 1, 3000, 0, 0, 'Gug Fatcandle - Cast \'Fireb
 
 -- Monstersay
 REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
-VALUES (62, 0, 100, 0, 12, 'Kobold Geomancer', 'You no take (fat) candle!', NULL, NULL, NULL, NULL);
+VALUES (62, 0, 100, 0, 12, 'Gug Fatcandle', 'You no take (fat) candle!', NULL, NULL, NULL, NULL);
 
 -- Magic Candle
 REPLACE INTO `loot_creatures` (`entryid`, `itemid`, `normal10percentchance`, `normal25percentchance`, `heroic10percentchance`, `heroic25percentchance`, `mincount`, `maxcount`) 
