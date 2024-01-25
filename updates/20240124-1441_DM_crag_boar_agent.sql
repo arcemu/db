8f1af8c9c6f11257e61866d995de29c7302bb71f
cressidagp <gcpigni@gmail.com>
2024-01-25 10:41:41 -0300

    Fixed Crag Boar, Large Crag Boar and Scarred Crag Boar 'Rushing Charge' spellid and chance.

---
+++ updates/20240124-1441_DM_crag_boar_agent.sql
@@ -0,0 +1,3 @@
+UPDATE ai_agents SET chance = 50, spell = 6268, comment = 'Crag Boar - Cast \'Rushing Charge\'' WHERE entry = 1125;
+UPDATE ai_agents SET chance = 50, spell = 6268, comment = 'Large Crag Boar - Cast \'Rushing Charge\'' WHERE entry = 1126;
+UPDATE ai_agents SET chance = 50, spell = 6268, comment = 'Elder Crag Boar - Cast \'Rushing Charge\'' WHERE entry = 1127;
