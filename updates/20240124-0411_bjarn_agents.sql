d94f11affec22fbc5cfee8a96eeb52ce3a13ef18
cressidagp <cressidagp@gmail.com>
2024-01-25 00:12:12 -0300

    Removed 'Bjarn' incorrect agents.

---
+++ updates/20240124-0411_bjarn_agents.sql
@@ -0,0 +1,3 @@
+DELETE FROM ai_agents WHERE entry = 1130 AND ( spell = 5116 OR spell = 9482 OR spell = 13549 OR spell = 25771 );
+UPDATE ai_agents SET spell = 3130, comment = 'Bjarn - Cast \'Ice Claw\'' WHERE entry = 1130 AND spell = 3130;
+UPDATE ai_agents SET spell = 3147, comment = 'Bjarn - Cast \'Rend Flesh\'' WHERE entry = 1130 AND spell = 3147;
