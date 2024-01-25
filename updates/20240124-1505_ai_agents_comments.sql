12a97bc0a7d255fcbfb43bdd51128cf0bbf5ac2a
cressidagp <cressidagp@gmail.com>
2024-01-25 11:05:36 -0300

    Added some remainders to agents with spell replacement since they need a force cast.

---
+++ updates/20240124-1505_ai_agents_comments.sql
@@ -0,0 +1,4 @@
+UPDATE ai_agents SET comment = 'Morgaine the Sly - Cast \'Gouge (1773)\'' WHERE entry = 99 AND spell = 12540;
+UPDATE ai_agents SET comment = 'Morgan the Collector - Cast \'Gouge (1776)\'' WHERE entry = 473 AND spell = 12540;
+UPDATE ai_agents SET comment = 'Defias Cutpurse - Cast \'Backstab (53)\'' WHERE entry = 94 AND spell = 7159;
+UPDATE ai_agents SET comment = 'Vejrek - Cast \'Sunder Amor (7386)\'' WHERE entry = 6113 AND spell = 11971;
