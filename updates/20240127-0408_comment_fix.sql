14b825c81563d47dc0fb7ebb7915502741c8fce8
cressidagp <cressidagp@gmail.com>
2024-01-27 00:10:03 -0300

    Typos and other minor fixes at ai_agents.

---
+++ updates/20240127-0408_comment_fix.sql
@@ -0,0 +1,4 @@
+UPDATE ai_agents SET comment = 'Morgaine the Sly - Cast \'Gouge (1776)\'' WHERE entry = 99 AND spell = 12540;
+UPDATE ai_agents SET spell = 9532, comment = 'Frostmane Seer - Cast \'Lightning Bolt\'' WHERE entry = 1397 AND spell = 9534;
+UPDATE ai_agents SET comment = 'Frostmane Seer - Cast \'Lightning Shield\'' WHERE entry = 1397 AND spell = 324;
+UPDATE ai_agents SET chance = 15, targettype_overwrite = -1, cooldown_overwrite = 14000 WHERE entry = 40 AND spell = 6016;
