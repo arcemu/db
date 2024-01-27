7af5e95dd814678bc33ed3f4a0f3b06f818e9995
cressidagp <cressidagp@gmail.com>
2024-01-27 12:33:25 -0300

    Apparently AT are stricktly handled at dbc.

--- updates/20240127-1311_the_defias_brotherhood_at.sql
+++
@@ -1,2 +0,0 @@
-REPLACE INTO `areatriggers` (`entry`, `type`, `map`, `screen`, `name`, `position_x`, `position_y`, `position_z`, `orientation`, `required_honor_rank`, `required_level`) 
-VALUES (78, 1, 36, 36, 'DeadMines Entrance', -14.5732, -385.475, 62.4561, 1.5708, 0, 10);
