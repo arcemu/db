f5dbb8c197047cc953a9454dc816fa2d5f54aa3b
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2024-01-29 03:23:11 +0100

    Typo

--- updates/20240129-0135_halaa_npcs.sql
+++ updates/20240129-0135_halaa_npcs.sql
@@ -1,5 +1,5 @@
 -- Driaan. What was she doing in Halaa even?!
-DELETE FROM creature_spawn WHERE entry IN ( 16744 );
+DELETE FROM creature_spawns WHERE entry IN ( 16744 );
 
 -- Remove Halaa NPC spawns as they are spawned by script
 DELETE FROM creature_spawns WHERE entry IN ( 18817, 18816, 18822, 18821, 21487, 21484, 21485, 21474, 21488, 21483 );
