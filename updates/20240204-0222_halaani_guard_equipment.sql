129a3f075fd960ed73f5cd216f68f689170264ae
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2024-02-04 03:23:15 +0100

    Halanni Guard equipment

---
+++ updates/20240204-0222_halaani_guard_equipment.sql
@@ -0,0 +1,5 @@
+-- Alliance Halaani Guard
+UPDATE creature_proto SET item1_display_id = 27407, item2_display_id = 24331, item3_display_id = 2551 WHERE entry = 18256;
+
+-- Horde Halaani Guard
+UPDATE creature_proto SET item1_display_id = 27405, item2_display_id = 27406, item3_display_id = 5259 WHERE entry = 18192;
