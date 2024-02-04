2d3845095f9019ea24a7bd58250e095c48e3cc9c
dfighter1985 <dfighter1985@dont.send.emails.to.me>
2024-02-04 01:37:42 +0100

    Zangarmarsh Twin Spire Ruins graveyard PvP banner factions

---
+++ updates/20240204-0031_zm_pvp_gy_banner_factions.sql
@@ -0,0 +1,8 @@
+-- Zangarmarsh Twin Spire ruins graveyard banner Alliance
+UPDATE gameobject_proto SET faction = 1739 WHERE entry = 182527;
+
+-- Zangarmarsh Twin Spire ruins graveyard banner Horde
+UPDATE gameobject_proto SET faction = 1740 WHERE entry = 182528;
+
+-- Zangarmarsh Twin Spire ruins graveyard banner Neutral
+UPDATE gameobject_proto SET faction = 1742 WHERE entry = 182529;
