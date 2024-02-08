761f3afddd6dc956533e70a7e09e3f3f741861f5
cressidagp <cressidagp@gmail.com>
2024-02-08 01:56:55 -0300

    Added 'Muad' agents.

---
+++ updates/20240208-0556_muad_agents.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (1910, 4, 4, 0, 15, 0, 332, 2, 4, 12000, 0.50, 0, 'Muad - Cast \'Healing Wave\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (1910, 4, 4, 0, 15, 0, 2607, 8, -1, 5000, 0, 0, 'Muad - Cast \'Shock\'');
