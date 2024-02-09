bd4bd3df7bc86aba6859e924f69494e15154116b
cressidagp <cressidagp@gmail.com>
2024-02-09 02:14:37 -0300

    Typo and rename.

--- updates/20240209-0549_kul_tiras_marine_agent.sql
+++ updates/20240209-0549_kul_tiras_marine_agent.sql
@@ -1,2 +1,2 @@
-UPDATE ai_agents SET cooldown_overwrite = 8000, comment = 'Kul Tiras Marine - Cast \'Shield Bash\'' WHERE entry = 3128 and spell = 11972;
+UPDATE ai_agents SET cooldown_overwrite = 8000, comment = 'Kul Tiras Marine - Cast \'Shield Bash\'' WHERE entry = 3129 and spell = 11972;
 UPDATE creature_proto SET spell1 = 11972 WHERE entry = 3129;
