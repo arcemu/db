054507187d68b87793503b1507594a9816753ea6
cressidagp <cressidagp@gmail.com>
2024-02-10 12:02:27 -0300

    Added some agent comments.

---
+++ updates/20240210-1603_ia_agents_comments.sql
@@ -0,0 +1,12 @@
+UPDATE ai_agents SET comment = 'Scorpid Worker - Cast \'Weak Poison\'' WHERE entry = 3124 AND spell = 6751;
+UPDATE ai_agents SET comment = 'Yarrog Baneshadow - Cast \'Corruption\'' WHERE entry = 3183 AND spell = 172;
+UPDATE ai_agents SET comment = 'Yarrog Baneshadow - Cast \'Immolate\'' WHERE entry = 3183 AND spell = 348;
+UPDATE ai_agents SET comment = 'Frostmane Novice - Cast \'Weak Frostbolt\'' WHERE entry = 946 AND spell = 6949;
+UPDATE ai_agents SET comment = 'Griknir the Cold - Cast \'Frostmane Strenght\'' WHERE entry = 808 AND spell = 6957;
+UPDATE ai_agents SET comment = 'Vejrek - Cast \'Defensive Stance\'' WHERE entry = 6113 AND spell = 7164;
+UPDATE ai_agents SET comment = 'Rockjaw Ambusher - Cast \'Backstab\'' WHERE entry = 1116 AND spell = 7159;
+UPDATE ai_agents SET comment = 'Rockjaw Skullthumper - Cast \'Head Crack\'' WHERE entry = 1115 AND spell = 3148;
+UPDATE ai_agents SET comment = 'Rockjaw Bonesnapper - Cast \'Knockdown\'' WHERE entry = 1117 AND spell = 5164;
+UPDATE ai_agents SET comment = 'Rockjaw Backbreaker - Cast \'Frenzy\'' WHERE entry = 1118 AND spell = 3019;
+UPDATE ai_agents SET comment = 'Leper Gnome - Cast \'Decayed Strenght\'' WHERE entry = 1211 AND spell = 6951;
+UPDATE ai_agents SET comment = 'Sarkoth - Cast \'Poison\'' WHERE entry = 3281 AND spell = 11918;
