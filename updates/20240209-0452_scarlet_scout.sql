32009c3aa0a07adeb5d98c74020ff0f2a3a64a1a
cressidagp <cressidagp@gmail.com>
2024-02-10 00:37:14 -0300

    Typo.

--- updates/20240209-0452_scarlet_scout.sql
+++ updates/20240209-0452_scarlet_scout.sql
@@ -2,7 +2,7 @@
 DELETE FROM ai_agents WHERE entry = 4281 and type = 4 and spell = 6660;
 
 -- since ranged agent are not working, enable ranged at creature_proto
-UPDATE creature_proto SET can_ranged = 1 WHERE entry 4281;
+UPDATE creature_proto SET can_ranged = 1 WHERE entry = 4281;
 
 -- just let this be now, will work as an spell agent
 UPDATE ai_agents SET event = 0, comment = 'Scarlet Scout - Cast \'Fire Shoot\'' WHERE entry = 4281 AND spell = 6979;
