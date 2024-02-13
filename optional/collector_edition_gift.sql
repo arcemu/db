fe5d8ea88657fd96dd712b18cb2345c68e487da5
cressidagp <cressidagp@gmail.com>
2024-02-13 14:45:41 -0300

    Added optional folder with Collector Edition Gifts.

---
+++ optional/collector_edition_gift.sql
@@ -0,0 +1,61 @@
+--
+-- Collector Edition Gifts
+--
+
+DELETE FROM `playercreateinfo_items` WHERE `protoid` IN ( 14646, 14647, 14648, 14649, 14650, 14651 );
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (1, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (9, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (18, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (25, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (34, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (38, 14646, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (56, 14646, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (2, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (11, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (19, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (30, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (39, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (57, 14649, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (3, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (10, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (12, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (20, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (26, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (58, 14647, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (4, 14648, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (15, 14648, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (21, 14648, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (27, 14648, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (42, 14648, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (59, 14648, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (5, 14651, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (22, 14651, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (28, 14651, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (35, 14651, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (40, 14651, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (60, 14651, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (6, 14650, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (16, 14650, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (31, 14650, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (43, 14650, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (61, 14650, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (7, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (23, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (36, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (41, 14647, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (62, 14647, 24, 1);
+
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (8, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (17, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (24, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (29, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (32, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (37, 14649, 24, 1);
+INSERT INTO `playercreateinfo_items` (`indexid`, `protoid`, `slotid`, `amount`) VALUES (63, 14649, 24, 1);
