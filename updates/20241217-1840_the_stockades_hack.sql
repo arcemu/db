0c16bd3b44fdcb829f2e45de257e8802ce813260
cressidagp <cressidagp@gmail.com>
2024-12-17 18:41:37 -0300

    Stockades: imposible to play with all these mobs walking through walls, changed some creature faction to neutral to make it more playable.

---
+++ updates/20241217-1840_the_stockades_hack.sql
@@ -0,0 +1,14 @@
+-- Defias Prisioner (1706)
+UPDATE creature_spawns SET faction = 36 WHERE id IN ( 12349, 12351, 12356 );
+
+-- Defias Captive (1707)
+UPDATE creature_spawns SET faction = 36 WHERE id IN ( 12361, 12369, 12367, 12366, 12368, 12372, 12373, 12376 );
+
+-- Defias Inmate (1708)
+UPDATE creature_spawns SET faction = 36 WHERE id IN ( 12391, 12393, 12394, 12389, 12380 );
+
+-- Defias Convict (1711)
+UPDATE creature_spawns SET faction = 36 WHERE id IN ( 12419, 12421, 12414 );
+
+-- Defias Insurgent (1715)
+UPDATE creature_spawns SET faction = 36 WHERE id IN ( 12437, 12438, 12439 );
