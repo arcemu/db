a33126791c5da082968efb88ef9390998efb2dd6
cressidagp <cressidagp@gmail.com>
2025-06-21 13:33:49 -0300

    Added 'Baltharus the Warborn' monstersay texts.

---
+++ updates/20250621-1335_baltharus_monstersay.sql
@@ -0,0 +1,6 @@
+-- Baltharus the Warborn: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (39751, 0, 100, 0, 14, 'Baltharus the Warborn', 'Ah, the entertainment has arrived.', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (39751, 5, 100, 0, 14, 'Baltharus the Warborn', 'I... didn\'t see that coming....', NULL, NULL, NULL, NULL);
