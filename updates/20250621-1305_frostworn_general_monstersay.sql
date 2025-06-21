71c65ff2a39f4cfd9f22bd92f60c1c95c8c7bb85
cressidagp <cressidagp@gmail.com>
2025-06-21 13:03:25 -0300

    Added 'Frostworn General' monstersay texts.

---
+++ updates/20250621-1305_frostworn_general_monstersay.sql
@@ -0,0 +1,6 @@
+-- Frostworn General: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36723, 0, 100, 0, 14, 'Frostworn General', 'You are not worthy to face the Lich King!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36723, 5, 100, 0, 14, 'Frostworn General', 'Master, I have failed...', NULL, NULL, NULL, NULL);
