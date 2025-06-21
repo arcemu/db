036edecaa0e39b1575488d7e682957f36ae73794
cressidagp <cressidagp@gmail.com>
2025-06-21 12:56:46 -0300

    Added 'Captain Falric' monstersay texts.

---
+++ updates/20250621-1256_captain_falric_monstersay.sql
@@ -0,0 +1,6 @@
+-- Captain Falric: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (38112, 0, 100, 0, 14, 'Falric', 'Men, women, and children... None were spared the master\'s wrath. Your death will be no different.', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (38112, 5, 100, 0, 14, 'Falric', 'Marwyn, finish them...', NULL, NULL, NULL, NULL);
