eea947e4170cddefd3eb233dcb7cabcbbfdfc397
cressidagp <cressidagp@gmail.com>
2025-06-21 13:00:31 -0300

    Added 'Captain Marwyn' monstersay texts.

---
+++ updates/20250621-1300_captain_marwyn_monstersay.sql
@@ -0,0 +1,6 @@
+-- Captain Marwyn: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (38113, 0, 100, 0, 14, 'Marwyn', 'Death is all that you will find here', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (38113, 5, 100, 0, 14, 'Marwyn', 'Yes... Run... Run to meet your destiny... Its bitter, cold embrace, awaits you.', NULL, NULL, NULL, NULL);
