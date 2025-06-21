f7f0ea004987480e8db2cc3ccba0ea0f1c1e033b
cressidagp <cressidagp@gmail.com>
2025-06-21 12:28:11 -0300

    Added 'Forgemaster Garfrost' monstersay texts.

---
+++ updates/20250621-1229_forgemaster_garfrost_monstersay.sql
@@ -0,0 +1,6 @@
+-- Forgemaster Garfrost: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36494, 0, 100, 0, 14, 'Forgemaster Garfrost', 'Tiny creatures under feet, you bring Garfrost something good to eat!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36494, 5, 100, 0, 14, 'Forgemaster Garfrost', 'Garfrost hope giant underpants clean. Save boss great shame. For later.', NULL, NULL, NULL, NULL);
