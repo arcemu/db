a601b9e852875745cee21af47713fe68d26c0036
cressidagp <cressidagp@gmail.com>
2025-06-21 12:14:22 -0300

    Added ´Bronjahm´ monstersay texts.

---
+++ updates/20250621-1211_bronjahm_monstersay.sql
@@ -0,0 +1,6 @@
+-- Bronjahm: monstersay OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36497, 0, 100, 0, 14, 'Bronjahm', 'Finally, a captive audience!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36497, 5, 100, 0, 14, 'Bronjahm', 'The Devourer awaits...', NULL, NULL, NULL, NULL);
