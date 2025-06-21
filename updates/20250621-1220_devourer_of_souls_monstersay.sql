84e6b9e922d052e0df49fcf88da23c289c64ae01
cressidagp <cressidagp@gmail.com>
2025-06-21 12:20:56 -0300

    Added 'Devourer of Souls' monstersay texts.

---
+++ updates/20250621-1220_devourer_of_souls_monstersay.sql
@@ -0,0 +1,6 @@
+-- Devourer Of Souls: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36502, 0, 100, 0, 14, 'Devourer Of Souls', 'You dare look upon the host of souls? I SHALL DEVOUR YOU WHOLE!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36502, 5, 100, 0, 14, 'Devourer Of Souls', 'The swell of souls will not be abated! You only delay the inevitable.', NULL, NULL, NULL, NULL);
