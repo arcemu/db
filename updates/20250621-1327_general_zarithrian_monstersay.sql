0d9bedf53855adc14527ba9a5fd1ac8b5e10874e
cressidagp <cressidagp@gmail.com>
2025-06-21 13:26:00 -0300

    Added 'General Zarithrian' monstersay texts.

---
+++ updates/20250621-1327_general_zarithrian_monstersay.sql
@@ -0,0 +1,6 @@
+-- General Zarithrian: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (39746, 0, 100, 0, 14, 'General Zarithrian', 'Alexstrasza has chosen capable allies.... A pity that I must END YOU!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (39746, 5, 100, 0, 14, 'General Zarithrian', 'HALION! I....', NULL, NULL, NULL, NULL);
