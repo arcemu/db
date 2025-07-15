bb5dc2606128e671746bae19fb6ec0d4419bc35d
cressidagp <cressidagp@gmail.com>
2025-07-15 10:16:43 -0300

    Added Lady Deathwhisper monstersay. Preparation for scripting.

---
+++ updates/20250715-1013_ICC_lady_deathwhisper.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (36855, 0, 100, 0, 14, 'Lady Deathwhisper', 'What is this disturbance? You dare trespass upon this hallowed ground? This shall be your final resting place!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (36855, 5, 100, 0, 14, 'Lady Deathwhisper', 'All part of the Master\'s plan.... Your end is inevitable....', NULL, NULL, NULL, NULL);
