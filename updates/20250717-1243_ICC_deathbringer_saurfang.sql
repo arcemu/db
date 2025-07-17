42c247034cc83f96e3cdf10967fdb3d9bd3196af
cressidagp <cressidagp@gmail.com>
2025-07-17 12:44:45 -0300

    Added Deathbringer Saurfang monstersay.

---
+++ updates/20250717-1243_ICC_deathbringer_saurfang.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (37813, 0, 100, 0, 14, 'Deathbringer Saurfang', 'BY THE MIGHT OF THE LICH KING!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (37813, 5, 100, 0, 14, 'Deathbringer Saurfang', 'I... Am... Released...', NULL, NULL, NULL, NULL);
