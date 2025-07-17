758c89f3a4981367fe2ad716cd73615b5d08a83d
cressidagp <cressidagp@gmail.com>
2025-07-17 16:54:38 -0300

    ICC: fixed Alliance vendor(s) phase at Deathbringer rise.

--- updates/20250717-1637_ICC_deathbringer_rise_horde_vendors.sql
+++ updates/20250717-1637_ICC_deathbringer_rise_horde_vendors.sql
@@ -1,5 +1,5 @@
 -- ICC: fixes 'Aphotecary Candith Tomas' phase
 UPDATE `creature_spawns` SET `phase` = 64 WHERE entry = 37935;
 
--- ICC: fixes 'Morgan Dayblaze' phase ()
+-- ICC: fixes 'Morgan Dayblaze' phase
 UPDATE `creature_spawns` SET `phase` = 64 WHERE entry = 37936;
---
+++ updates/20250717-1654_ICC_deathbringer_rise_alliance_vendors.sql
@@ -0,0 +1,5 @@
+-- ICC: fixes 'Brazie Getz' phase
+UPDATE `creature_spawns` SET `phase` = 128 WHERE entry = 37904;
+
+-- ICC: fixes 'Shely Steelbowels' phase
+UPDATE `creature_spawns` SET `phase` = 128 WHERE entry = 37903;
