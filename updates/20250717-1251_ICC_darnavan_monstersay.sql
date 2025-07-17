2ced99ec8f3089738e0abaa601b105ea88283fa9
cressidagp <cressidagp@gmail.com>
2025-07-17 12:51:35 -0300

    Added Darnavan(s) monstersay (quest deprogramming at ICC).

---
+++ updates/20250717-1251_ICC_darnavan_monstersay.sql
@@ -0,0 +1,5 @@
+-- Darnavan(s) monstersay onEnterCombat (quest deprograming at ICC)
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (38472, 0, 100, 0, 14, 'Darnavan', 'Die, intruders! None shall interfere with the Cult\'s plans!', NULL, NULL, NULL, NULL);
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (38485, 0, 100, 0, 14, 'Darnavan', 'Die, intruders! None shall interfere with the Cult\'s plans!', NULL, NULL, NULL, NULL);
