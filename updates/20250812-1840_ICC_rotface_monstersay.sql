9a4e48039ccff8212ab9f331f7c8758c72c57a5a
cressidagp <cressidagp@gmail.com>
2025-08-02 18:40:55 -0300

    Added Rotface monstersay: onAggro, onDeath.

---
+++ updates/20250812-1840_ICC_rotface_monstersay.sql
@@ -0,0 +1,6 @@
+-- Rotface: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36627, 0, 100, 0, 14, 'Rotface', 'Weeeeee!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36627, 5, 100, 0, 14, 'Rotface', 'Bad news, daddy...', NULL, NULL, NULL, NULL);
