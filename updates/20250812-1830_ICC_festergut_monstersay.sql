797b6ac86b9c88213d51b6fbb489d2fb1de9e629
cressidagp <cressidagp@gmail.com>
2025-08-02 18:34:46 -0300

    Added Festergut monstersay: onAggro, on Death.

---
+++ updates/20250812-1830_ICC_festergut_monstersay.sql
@@ -0,0 +1,6 @@
+-- Festergut: OnCombat, OnDeath
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36626, 0, 100, 0, 14, 'Festergut', 'Fun time?', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36626, 5, 100, 0, 14, 'Festergut', 'Da ... Ddy...', NULL, NULL, NULL, NULL);
