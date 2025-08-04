a789012ed7c9736f150198e2468342777ef28a57
cressidagp <cressidagp@gmail.com>
2025-08-04 08:27:50 -0300

    ICC: added Professor Putricide monstersay: OnAggro, onDeath.

---
+++ updates/20250803-2243_ICC_professor_putricide_monstersay.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (36678, 0, 100, 0, 14, 'Professor Putricide', 'Good news, everyone! I think I\'ve perfected a plague that will destroy all life on Azeroth!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36678, 5, 100, 0, 14, 'Professor Putricide', 'Bad news, everyone... I don\'t think I\'m going to make it...', NULL, NULL, NULL, NULL);
