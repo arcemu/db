005cf7784f4fb46928b5171cb645a8ac47996723
cressidagp <cressidagp@gmail.com>
2025-08-04 08:32:32 -0300

    ICC: added Sindragosa monstersay: onAggro, onDeath

---
+++ updates/20250804-0832_ICC_sindragosa_monstersay.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`)
+VALUES (36853, 0, 100, 0, 14, 'Sindragosa', 'You are fools to have come to this place. The icy winds of Northrend will consume your souls!', NULL, NULL, NULL, NULL);
+
+REPLACE INTO `npc_monstersay` (`entry`, `event`, `chance`, `language`, `type`, `monstername`, `text0`, `text1`, `text2`, `text3`, `text4`) 
+VALUES (36853, 5, 100, 0, 14, 'Sindragosa', 'Free... at last....', NULL, NULL, NULL, NULL);
