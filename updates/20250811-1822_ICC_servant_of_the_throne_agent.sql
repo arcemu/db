249d62c95d82fba70122ee8e7651d750eb8ad245
cressidagp <cressidagp@gmail.com>
2025-08-11 18:22:46 -0300

    ICC: added 'Servant of the Throne' agent. They should cast 'Glacial Blast'. Needs a dummy effect.

---
+++ updates/20250811-1822_ICC_servant_of_the_throne_agent.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36724, 4, 4, 0, 25, 0, 71029, 7, -1, 3000, 0, 0, 'Servant of the Throne - Cast \'Glacial Blast\'');
