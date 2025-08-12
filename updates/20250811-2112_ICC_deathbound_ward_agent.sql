57a69671b96ba9c2c3fd64024cbeea21c666bafd
cressidagp <cressidagp@gmail.com>
2025-08-11 21:14:19 -0300

    ICC: added 'Deathbound Ward' agents.

---
+++ updates/20250811-2112_ICC_deathbound_ward_agent.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (37007, 4, 4, 2, 25, 0, 71021, 7, -1, 3000, 0, 0, 'ICC: Deathbound Ward - Cast \'Saber Lash\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (37007, 4, 4, 2, 33, 0, 71022, 10, -1, 6000, 0, 0, 'ICC: Deathbound Ward - Cast \'Disrupting Shout\'');
