4afb783052175c106c077b7552a384b7ccfa91b2
cressidagp <cressidagp@gmail.com>
2025-08-11 21:27:41 -0300

    ICC: added creature_proto spells to Damned, Servant of the Throne, Ancient Skeletal Soldier and Deathbound Ward. Possession its a cool feature and its a quick way to see in game what spell a creature should cast.

---
+++ updates/20250811-2121_ICC_creature_proto_spells.sql
@@ -0,0 +1,11 @@
+-- The Damned:
+UPDATE creature_proto SET spell1 = 70960, spell2 = 70961 WHERE entry = 37011;
+
+-- Servant of the Throne:
+UPDATE creature_proto SET spell1 = 71029 WHERE entry = 36724;
+
+-- Ancient Skeletal Soldier:
+UPDATE creature_proto SET spell1 = 70964 WHERE entry = 37012;
+
+-- Deathbound Ward:
+UPDATE creature_proto SET spell1 = 71021, spell2 = 71022 WHERE entry = 37007;
