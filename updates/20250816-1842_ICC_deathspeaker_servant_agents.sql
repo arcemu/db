4de3c972800c09d2c83e7dd6382e3ac9a6784212
cressidagp <cressidagp@gmail.com>
2025-08-16 18:43:46 -0300

    ICC: added 'Deathspeaker Servant' agents.

---
+++ updates/20250816-1842_ICC_deathspeaker_servant_agents.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36805, 4, 4, 0, 30, 0, 69576, 8, 1, 3000, 0, 0, 'ICC: Deathspeaker Servant - Cast \'Chaos Bolt\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36805, 4, 4, 0, 30, 0, 69405, 8, 1, 3000, 0, 0, 'ICC: Deathspeaker Servant - Cast \'Consuming Shadows\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36805, 4, 4, 0, 30, 0, 69404, 8, 1, 3000, 0, 0, 'ICC: Deathspeaker Servant - Cast \'Curse of Agony\'');
