f7f4f9102274dd05033984bdb3105a4ec99b8d88
cressidagp <cressidagp@gmail.com>
2025-08-16 18:08:45 -0300

    ICC: added 'Deathspeaker Disciples' agents.

---
+++ updates/20250816-1807_ICC_deathspeaker_disciples_agents.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36807, 4, 4, 0, 30, 0, 69387, 8, 1, 3000, 0, 0, 'ICC: Deathspeaker Disciple - Cast \'Shadow Bolt\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36807, 4, 4, 0, 30, 0, 69389, 2, 4, 15000, 0.66, 0, 'ICC: Deathspeaker Disciple - Cast \'Shadow Mend\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36807, 4, 4, 0, 30, 0, 69391, 10, 4, 15000, 0.66, 0, 'ICC: Deathspeaker Disciple - Cast \'Dark Blessing\'');
