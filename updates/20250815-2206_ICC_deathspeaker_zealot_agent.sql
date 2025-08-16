639ec162dfa4b03b10bdac0dc67c855d8cb533ad
cressidagp <cressidagp@gmail.com>
2025-08-15 22:07:14 -0300

    ICC: added 'Deathspeaker Zealot' agent. It will cast 'Shadow Cleave'.

---
+++ updates/20250815-2206_ICC_deathspeaker_zealot_agent.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36808, 4, 4, 0, 25, 0, 69492, 8, -1, -1, 0, 0, 'ICC: Deathspeaker Zealot - Cast \'Shadow Cleave\'');
