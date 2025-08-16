a78a00969d6b511c31cd32eef138eed0cdb0b9bb
cressidagp <cressidagp@gmail.com>
2025-08-16 19:13:20 -0300

    ICC: added 'Nerubar Broodkeeper' agents. The first level its complete now. Will get to the top eventualy.

---
+++ updates/20250816-1912_ICC_nerubar_broodkeeper_agents.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36725, 4, 4, 0, 30, 0, 70965, 8, 1, 3000, 0, 0, 'ICC: Nerubar Broodkeeper - Cast \'Crypt Scarabs\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36725, 4, 4, 0, 30, 0, 70980, 1, 1, 20000, 0, 0, 'ICC: Nerubar Broodkeeper - Cast \'Web Wrap\'');
+
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (36725, 4, 4, 0, 30, 0, 71020, 2, 4, 5000, 0.75, 0, 'ICC: Nerubar Broodkeeper - Cast \'Dark Mending\'');
