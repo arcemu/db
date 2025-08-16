2bc136d24be24ef7cc7581fa5d09f7295988942e
cressidagp <cressidagp@gmail.com>
2025-08-16 11:52:51 -0300

    ICC: added 'The Damned' agent for 'Bone Flurry'. Also fixed hour on previous commit.

---
+++ updates/20250816-1152_ICC_the_damned_agent.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (37011, 4, 4, 0, 25, 0, 70960, 10, -1, 15000, 0, 0, 'The Damned - Cast \'Bone Flurry\'');
