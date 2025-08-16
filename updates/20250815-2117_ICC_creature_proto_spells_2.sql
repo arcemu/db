e27e854b4290b6261b060e873e4c257ac7f9274e
cressidagp <cressidagp@gmail.com>
2025-08-15 21:17:57 -0300

    ICC: added more creature_proto spells.

---
+++ updates/20250815-2117_ICC_creature_proto_spells_2.sql
@@ -0,0 +1,17 @@
+-- Nerubar Broodkeeper:
+UPDATE creature_proto SET spell1 = 70965, spell2 = 70980, spell3 = 71020, spell4 = 69887 WHERE entry = 36725;
+
+-- Deathspeaker Servant:
+UPDATE creature_proto SET spell1 = 69576, spell2 = 69405, spell3 = 69404 WHERE entry = 36805;
+
+--Deathspeaker Zealot:
+UPDATE creature_proto SET spell1 = 69492 WHERE entry = 36808;
+
+--Deathspeaker Attendant:
+UPDATE creature_proto SET spell1 = 69387, spell2 = 69355 WHERE entry = 36811;
+
+--Deathspeaker Disciple:
+UPDATE creature_proto SET spell1 = 69387, spell2 = 69391, spell3 = 69389 WHERE entry = 36807;
+
+--Deathspeaker High Priest:
+UPDATE creature_proto SET spell1 = 69483, spell2 = 69491 WHERE entry = 36829;
