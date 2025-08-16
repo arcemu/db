7dbc79f2964bb88faf030f8369ad8717822b4698
cressidagp <cressidagp@gmail.com>
2025-08-16 13:33:48 -0300

    Hotfix.

--- updates/20250816-1248_ICC_deathspeaker_attendant_agents.sql
+++ updates/20250816-1248_ICC_deathspeaker_attendant_agents.sql
@@ -1,5 +1,5 @@
 REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
-VALUES (36811, 4, 4, 0, 30, 0, 69387, 8, 1, 3000, 0, 0, 'Deathspeaker Attendant - Cast \'Shadow Bolt\'');
+VALUES (36811, 4, 4, 0, 30, 0, 69387, 8, 1, 3000, 0, 0, 'ICC: Deathspeaker Attendant - Cast \'Shadow Bolt\'');
 
 REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
-VALUES (36811, 4, 4, 0, 30, 0, 69355, 7, 2, 3000, 0, 0, 'Deathspeaker Attendant - Cast \'Shadow Nova\'');
+VALUES (36811, 4, 4, 0, 30, 0, 69355, 7, 2, 3000, 0, 0, 'ICC: Deathspeaker Attendant - Cast \'Shadow Nova\'');
