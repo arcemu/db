c037e2ca85a61cde328694712de111621d3f0e35
cressidagp <cressidagp@gmail.com>
2025-09-27 17:22:55 -0300

    Added: 'Ferocitas the Dreameater' agent. It should cast 'Thrash'

---
+++ updates/20250927-1713_ferocitas_agent.sql
@@ -0,0 +1,3 @@
+DELETE FROM ai_agents WHERE entry = 7234;
+REPLACE INTO `ai_agents` (`entry`, `instance_mode`, `type`, `event`, `chance`, `maxcount`, `spell`, `spelltype`, `targettype_overwrite`, `cooldown_overwrite`, `floatMisc1`, `Misc2`, `comment`) 
+VALUES (7234, 4, 4, 0, 20, 0, 12787, 8, -1, 2000, 0, 0, 'Ferocitas the Dream Eater - Cast \'Thrash\'');
