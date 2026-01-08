af5d12c38c9605eea5728f8ba512dc9db16721f1
cressidagp <cressidagp@gmail.com>
2026-01-08 13:55:16 -0300

    Delete DBW agent for cast Blood Mirror since i didnt realize that needs a dummy. Also fixed comments.

---
+++ updates/20260108-1354_darkfallen_blood_fix.sql
@@ -0,0 +1,3 @@
+DELETE FROM ai_agents WHERE entry = 37595 AND spell = 70450;
+UPDATE ai_agents SET comment = 'ICC: Darkfallen Blood Knight - Cast \'Unholy Strike\'' WHERE entry = 37595 AND spell = 70437;
+UPDATE ai_agents SET comment = 'ICC: Darkfallen Blood Knight - Cast \'Vampiric Aura\'' WHERE entry = 37595 AND spell = 71736;
